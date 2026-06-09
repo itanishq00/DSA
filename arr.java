1d632cbffcd4568e9d1dac48523e84cf1296d0a7
itanishq00 <chauhantanishq00@gmail.com>
2026-06-09 15:05:58 +0530

    Created new Array.java file to print the array of given number.

---
+++ arr.java
@@ -0,0 +1,11 @@
+
+public class arr {
+    public static void main(String[] args)
+    {
+        int ar[] = {5,10,15,20,25,30,6};
+        for (int i =0;i<ar.length;i++)
+        {
+            System.out.println(ar[i]);
+        }
+    }
+}
